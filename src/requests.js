cb1865651ff31393d4c6148ff4cb9b5400e7ec82
Suman <sumandavid2025@gmail.com>
2026-02-27 15:48:54 +0530

    Remove API key and use environment variables

--- src/requests.js
+++ src/requests.js
@@ -1,4 +1,4 @@
-const API_KEY = "59c93b939f290d23d7b45510e3357627";
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 
 const requests = {
     fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
